b440d28c63aa579e41542bf9bf594aaab65c1876
worace <horace.d.williams@gmail.com>
2015-02-18 14:05:26 -0700

    work so far from tdd workshop with cryptographer

---
+++ cryptographer.rb
@@ -0,0 +1,40 @@
+class Cryptographer
+  def encrypt(message) #"ad"
+    # find position of the character (a -> 0)
+    # find the new (shifted) position (0 -> 13)
+    # find the character at that new position (13 -> n)
+
+    # what can we do -- encrypt single characters
+    # how do i encrypt a multicharacter string?
+      # encrypt each character individually
+
+    encrypt_char(message[0]) # "a"
+  end
+
+  def encrypt_char(char)
+    pos = position(char) # 0
+    new_pos = rot(pos) # 13
+    new_char = character(new_pos) # "n"
+    new_char
+  end
+
+  def alphabet
+    ("a".."z").to_a
+  end
+
+  # finds 0-based numerical position
+  # of a character in the alphabet
+  def position(character)
+    alphabet.index(character)
+  end
+
+  # finds a character in the alphabet
+  # given its 0-based numerical position
+  def character(position)
+    alphabet[position]
+  end
+
+  def rot(position)
+    position + 13
+  end
+end
---
+++ cryptographer_test.rb
@@ -0,0 +1,55 @@
+require "minitest"
+require "minitest/autorun"
+require_relative "cryptographer"
+
+class CryptographerTest < Minitest::Test
+  def test_it_runs
+    assert true
+  end
+
+  def test_cryptographer_exists
+    assert Cryptographer
+  end
+
+  # Cryptographer can encrypt and decrypt a message
+  # using ROT13
+  # a -> n
+  # y -> l
+
+  def test_it_encrypts_a_single_letter
+    cryptographer = Cryptographer.new
+    assert_equal "n", cryptographer.encrypt("a")
+  end
+
+  def test_it_encrypts_2_letters
+    skip
+    cryptographer = Cryptographer.new
+    assert_equal "nq", cryptographer.encrypt_char("ad")
+  end
+
+  def test_it_encrypts_a_single_char
+    cryptographer = Cryptographer.new
+    assert_equal "n", cryptographer.encrypt_char("a")
+  end
+
+  def test_it_knows_the_alphabet
+    cryptographer = Cryptographer.new
+    abet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    assert_equal abet, cryptographer.alphabet
+  end
+
+  def test_it_finds_alphabetic_position_of_a_character
+    cryptographer = Cryptographer.new
+    assert_equal 0, cryptographer.position("a")
+  end
+
+  def test_it_rotates_a_position
+    cryptographer = Cryptographer.new
+    assert_equal 13, cryptographer.rot(0)
+  end
+
+  def test_it_finds_a_character_for_a_position
+    cryptographer = Cryptographer.new
+    assert_equal "n", cryptographer.character(13)
+  end
+end
